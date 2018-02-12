c3d70492fd9b7f3f073225d4d3c1cf97644f51a8
Kevin Neag <neagkv@gmail.com>
2018-02-12 11:57:30 -0500

    new project repo

---
+++ src/main/java/com/application/PortScanner/PortScannerApplication.java
@@ -0,0 +1,12 @@
+package com.application.PortScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortScannerApplication.class, args);
+	}
+}
