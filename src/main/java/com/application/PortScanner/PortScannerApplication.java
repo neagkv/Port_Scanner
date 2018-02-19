cc399d1223ee381b67353887e5bf2b9080992939
Kevin Neag <neagkv@gmail.com>
2018-02-19 12:11:36 -0500

    frame

--- src/main/java/com/application/PortScanner/PortScannerApplication.java
+++ src/main/java/com/application/PortScanner/PortScannerApplication.java
@@ -1,12 +1,17 @@
 package com.application.PortScanner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PortScannerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PortScannerApplication.class, args);
 	}
+
 }
