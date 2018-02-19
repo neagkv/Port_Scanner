1810ca547d30879981b19967b9bfad45d68dc19f
Kevin Neag <neagkv@gmail.com>
2018-02-19 07:56:27 -0500

    reddis

--- src/main/java/com/application/PortScanner/configuration/WebConfiguration.java
+++ src/main/java/com/application/PortScanner/configuration/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 
 /**
@@ -17,6 +18,15 @@ public class WebConfiguration {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+    }
+
+    @Bean
+    JedisConnectionFactory getJedisConnectionFactory(){
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName("localhost");
+        jedisConnectionFactory.setPort(6379);
+        return jedisConnectionFactory;
+
     }
 
 }
